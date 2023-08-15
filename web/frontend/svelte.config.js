d8af50f7efe23a5d4565de545bcd3177379d7bd7
Rupin Chheda <rupin.chheda@gmail.com>
2023-08-15 13:08:06 +0530

    Update svelte.config.js
    Added Warning Handler https://stackoverflow.com/questions/74974066/visible-non-interactive-elements-with-an-onclick-event-must-be-accompanied-by

--- web/frontend/svelte.config.js
+++ web/frontend/svelte.config.js
@@ -8,3 +8,11 @@ export default {
    */
   preprocess: vitePreprocess(),
 };
+
+const config = {
+  // ...
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith('a11y-')) return
+    handler(warning)
+  },
+}
